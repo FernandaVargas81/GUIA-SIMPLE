2247f327fb24bfdcee3a79504a7a9b93e88db765
FernandaVargas81 <kaiamfrv220822@gmail.com>
2026-07-23 16:02:52 -0500

    Delete inicio.js

--- inicio.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('search-input');
-    const searchContainer = searchInput?.parentElement;
-    if (searchInput && searchContainer) {
-        searchInput.addEventListener('focus', () => searchContainer.classList.add('scale-[1.01]', 'border-primary'));
-        searchInput.addEventListener('blur', () => searchContainer.classList.remove('scale-[1.01]', 'border-primary'));
-    }
-});
